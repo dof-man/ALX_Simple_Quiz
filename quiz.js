ba1fcfa70c79247402026ca7c580cc6ba2814019
onucheojo <mandofman@gmail.com>
2024-10-07 18:25:55 +0100

    fix quiz

--- quiz.js
+++ quiz.js
@@ -3,14 +3,20 @@
             const correctAnswer = "4";
             
             
-            const userAnswer = document.querySelector('input[name="quiz"]:checked');
+            const quizAnswer = document.querySelector('input[name="quiz"]:checked');
+            const userAnswer = quizAnswer.value
             
             
-            if (userAnswer.value === correctAnswer) {
+            if (userAnswer) {
                 
-                if (userAnswer.value === correctAnswer)
+                if (userAnswer === correctAnswer)
+                    
                      {
                     document.getElementById("feedback").textContent = "Correct! Well done.";
+                    console.log(userAnswer.value);
+                    console.log(userAnswer);
+                    console.log(correctAnswer)
+
                 } else {
                     document.getElementById("feedback").textContent = "That's incorrect. Try again!";
                 }
